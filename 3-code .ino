7efea5777b27178ab0f01d2b2d9e1b272ddd500a
Wafiifaw55 <85634065+Wafiifaw55@users.noreply.github.com>
2021-07-04 22:24:57 +0300

    Add files via upload

---
+++ 3-code .ino
@@ -0,0 +1,17 @@
+void setup() {
+  
+}
+
+void loop() {
+ analogWrite{9, 64};
+ delay{1000};
+ analogWrite{9, 127};
+ delay{1000};
+ analogWrite{9, 191};
+ delay{1000};
+ analogWrite{9, 255};
+ delay{1000};
+ analogWrite{9, 0};
+ delay{1000};
+
+}
